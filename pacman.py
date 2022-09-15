33dd1f13881a0c40181a311124efeb733d38d9ed
RikuNav <ricardonavarro2003@gmail.com>
2022-09-15 15:06:04 -0500

    Comentar código y funcionamiento

--- pacman.py
+++ pacman.py
@@ -9,15 +9,29 @@ Exercises
 5. Make the ghosts smarter.
 """
 
+
+# Librería random, importa choice para
+# la selección de un número random
 from random import choice
+
+# Librería turtle, que nos ayuda a manipular
+# objetos, para que lleven a cabo los movimientos
 from turtle import *
 
+# Librería freegames, que importará floor y vector,
+# los cuales,el primero te calcula un punto, desde la izquierda
+# dado un valor, o tamaño.
+# vector crea objetos de esa clase, con dirección y sentido
 from freegames import floor, vector
 
+# Score será el diccionario
 state = {'score': 0}
+# Estado del camino tanto pintado y sin pintar
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
+# velocidad
 aim = vector(5, 0)
+# Posición de pacman y fantasmas
 pacman = vector(-40, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
@@ -26,6 +40,10 @@ ghosts = [
     [vector(100, -160), vector(-5, 0)],
 ]
 # fmt: off
+# Tablero de 20 x 20 (es una matriz de 20 x 20)
+# El 1 representa un camino, mientras que los 0 son
+# las barreras, que ningun personaje puede llegar
+# a atravesar
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -50,6 +68,9 @@ tiles = [
 ]
 # fmt: on
 
+# Función que dibuja el tablero utilizando
+# las coordenadas en x y y
+
 
 def square(x, y):
     """Draw square using path at (x, y)."""
@@ -62,6 +83,10 @@ def square(x, y):
         path.forward(20)
         path.left(90)
 
+    # Avanza 20 puntos en el eje
+    # Luego se recorre 90 grados a la izquierda
+    # Aquí se crea el tablero
+
     path.end_fill()
 
 
@@ -71,6 +96,10 @@ def offset(point):
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
+
+# Función que revisa si hay caminos
+# disponibles para que un fantasma
+# pueda moverse
 
 
 def valid(point):
@@ -87,6 +116,8 @@ def valid(point):
 
     return point.x % 20 == 0 or point.y % 20 == 0
 
+# Crea realmeente el mundo, y define los colores que
+# tendran los caminos y las barreras
 
 def world():
     """Draw world using path."""
@@ -114,10 +145,22 @@ def move():
 
     clear()
 
+    # Llama a la función valid(), que validará
+    # si el punto al que se planea mover pacman,
+    # es disponible o no, si el valor del tile es 0,
+    # regresa false y no se moverá, en caso contrario
+    # se moverá
+
     if valid(pacman + aim):
         pacman.move(aim)
 
     index = offset(pacman)
+
+    # Si offset regresa un índice en el que la posición
+    # es 1, el tablero tendrá una posición de 2, volviendose
+    # pacman, se aumenta el score
+    # Se cambian las coordenadas  y se llamará a la función
+    # square() para actualizar la pintura del tablero
 
     if tiles[index] == 1:
         tiles[index] = 2
@@ -171,6 +214,10 @@ writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
 listen()
+
+# Va a declarar las teclas que van a llevar a cabo
+# el movimiento de pacman
+
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
